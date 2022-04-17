a9eb1e068a92bff355a0d1a5b30bcda5f2ccfd3e
Pierre Ehab <pieer44@gmail.com>
2022-04-18 01:35:29 +0200

    lasso regression is implemented. fixed normalization in nn.

--- models/nn.py
+++ models/nn.py
@@ -1,7 +1,8 @@
 # ANN code Here
 from matplotlib import pyplot as plt
+from pandas import read_csv
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from keras import layers, models
 from keras.layers import Convolution2D, Activation, MaxPooling2D, Dropout, Flatten, Dense
 from PreProcessing import preprocessing
@@ -11,17 +12,21 @@ from helper_functions import write_to_csv
 
 seed = 23
 train_data, test_data = preprocessing()
-
+test_data = test_data.drop(['ID'], axis=1)
 # Get X and Y
 
 x = train_data.drop(['Rented Bike Count'], axis=1).values
 y = train_data['Rented Bike Count'].values.reshape(-1, 1)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=seed)
 scaler = MinMaxScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
+scaler_fit = scaler.fit(x)
+x = scaler_fit.transform(x)
+test_scaler = MinMaxScaler()
+test_scaler_fit = test_scaler.fit(test_data)
+test_data = test_scaler_fit.transform(test_data)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=seed)
+
 
 model = Sequential()
 model.add(Dense(64, input_dim=12, activation='relu'))
@@ -34,7 +39,6 @@ model.summary()
 history = model.fit(x_train, y_train, epochs=100, validation_split=0.2, validation_data=(x_test, y_test))
 
 # Predict on test data
-print(test_data.shape)
-predictions = model.predict(test_data.drop(['ID'], axis=1))
+predictions = model.predict(test_data)
 # Save to csv file
-write_to_csv('predictedFromANN.csv', predictions)
+write_to_csv('predictedFromNN.csv', predictions)
