3b89287408025f94b334343de7d6a4ac4d3bc49a
Nermin Ezzy <48429652+Nemosimes@users.noreply.github.com>
2022-04-17 23:10:56 +0200

    ridge regression empty

---
+++ models/ridge_regression.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from helper_functions import write_to_csv
+from PreProcessing import preprocessing
+
+
+train_data, test_data = preprocessing()
+y = train_data['Rented Bike Count'].values.reshape(-1, 1)
+x = train_data.drop(['Rented Bike Count'], axis=1).values
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=24)
+
+reg = Ridge(alpha=1.0)
+reg.fit(x_train, y_train)
+y_pred = reg.predict(test_data.drop(['ID'], axis=1))
+write_to_csv('predictedFromRidgeReg.csv', y_pred)
+#print("MAE",mean_absolute_error(y_test,y_pred))
