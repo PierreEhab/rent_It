89db2d4ac17b1a1f10458d14bd651488f22281d9
Nermin Ezzy <48429652+Nemosimes@users.noreply.github.com>
2022-05-01 22:27:40 +0200

    edit

--- models/random_forest.py
+++ models/random_forest.py
@@ -13,13 +13,15 @@ y = train_data['Rented Bike Count'].values
 x = train_data.drop(['Rented Bike Count'], axis=1).values
 x = normalize_data(x)
 test_data = normalize_data(test_data)
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=24)
 randForest = RandomForestRegressor()
 randForest.fit(x, y)
 y_pred = randForest.predict(test_data)
 print(y_pred.shape)
 write_to_csv('predictions/predictedFromRandomForest.csv', y_pred)
 
-'''y_pred = randForest.predict((x_test))
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=24)
+randForest = RandomForestRegressor()
+randForest.fit(x_train, y_train)
+y_pred = randForest.predict((x_test))
 print("MAE", mean_absolute_error(y_test, y_pred))
-print("RMSE", math.sqrt(mean_squared_error(y_test, y_pred)))'''
+print("RMSE", math.sqrt(mean_squared_error(y_test, y_pred)))
--- models/ridge_regression.py
+++ models/ridge_regression.py
@@ -1,8 +1,10 @@
+import math
+
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from helper_functions import write_to_csv, normalize_data
 from PreProcessing import preprocessing
-from sklearn.metrics import  mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 train_data, test_data = preprocessing()
 test_data = test_data.drop(['ID'], axis=1)
@@ -12,9 +14,10 @@ x = normalize_data(x)
 test_data = normalize_data(test_data)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=24)
 
-reg = Ridge(alpha=1.0)
+reg = Ridge(alpha=0.005)
 reg.fit(x_train, y_train)
 y_pred = reg.predict(test_data)
 write_to_csv('predictions/predictedFromRidgeReg.csv', y_pred)
 y_pred = reg.predict(x_test)
 print("MAE",mean_absolute_error(y_test,y_pred))
+print("RMSE", math.sqrt(mean_squared_error(y_test, y_pred)))
